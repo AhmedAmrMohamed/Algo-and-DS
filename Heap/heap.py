7b98bd3f79baca24998ae657a22ad79b0c0db9c8
theunderd0g <ahmedbonumstelio@gmail.com>
2018-07-27 23:26:35 +0200

    v03
    Can convert a list into a heap object.
    added Heap sort

--- Heap/heap.py
+++ Heap/heap.py
@@ -1,27 +1,44 @@
 class Heap:
-    def __init__(self,max = True):
+    def __init__(self,list=None,max=True):
         self.li = [None]
         self.max=max
         self.size = 0
-        self.parent = lambda a:a//2
+        self.parent = lambda a: a//2 if a//2 > 0 else 1
         self.left   = lambda a:a<<1
         self.right  = lambda a:(a<<1)|1
         self.cmp    = lambda a,b:a>b if max else a<b
         # self.comp   = key
+        if list !=None:
+            self.constructHeap(list)
     def __str__(self):
         return str(self.li)
 
     def __repr__(self):
         return self.__str__()
 
+    def __next__(self):
+        if self.itr>=self.size:
+             raise StopIteration
+        self.itr+=1
+        return self.li[self.itr]
+
     def __len__(self):
         return self.size
+
+    def __iter__(self):
+        self.itr=0
+        return self
+
+    def empty(self):
+        return self.size == 0
+
     def swim(self,pos):
         li=self.li
         while(pos > 1 and self.cmp(li[pos],li[self.parent(pos)])):
             parent_pos=self.parent(pos)
             li[pos],li[parent_pos] = li[parent_pos],li[pos]
             pos=parent_pos
+
     def sink(self,pos):
         li=self.li
         left=self.left
@@ -56,9 +73,22 @@ class Heap:
             return None
         return self.li[1]
 
+    def constructHeap(self,li):
+        for i in li:
+            self.li.append(i)
+        self.size = len(self.li)-1
+        pos = self.parent(self.size)
+        while pos>0:
+            self.sink(pos)
+            pos-=1
 
 
-
+    def sort(list,max=True):
+        tmp = Heap(list,max)
+        li=[]
+        for i in range(tmp.size):
+            li.append(tmp.pop())
+        return li
 
 
 import random as ran
@@ -71,7 +101,7 @@ class Testing:
         self.testing()
     def fill(self,n):
         li=[ran.randint(-n,n)*self.sign for i in range(n)]
-        x=Heap(self.max)
+        x=Heap(max=self.max)
         for i in li:
             x.insert(i)
         return x,li
@@ -82,15 +112,31 @@ class Testing:
             if i!=x.pop():
                 return False
         return True
+    def checksorting(self,x,li):
+        li.sort(reverse=self.max)
+        x=Heap.sort(list=li)
+        for i,ii in zip(li,x):
+            if i!=ii:
+                return False
+        return True
 
     def testing(self):
         ret=[]
         for test in range(self.tests):
             leng = ran.randint(1,self.max_lenght)
             x,l=self.fill(leng)
-            passed='passed' if self.pop(x,l) else 'failed'
-            print('test :{} {} on len = {}'.format(test,passed,leng))
-            if passed =='failed':
+            fillp='passed' if self.pop(x,l) else 'failed'
+            sortp='passed' if self.checksorting(x,l) else 'failed'
+            print('test :{} len : {} poping : {} sorting : {}'.format(test,leng,fillp,sortp))
+            if fillp =='failed':
                 print(l)
-                ret.append(test)
+                ret.append((test,'pop'))
+            elif sortp =='failed':
+                print(l)
+                ret.append((test,'sort'))
         print(ret)
+# # li = [ran.randint(1,10) for i in range(10)]
+# print(li)
+# x=Heap()
+# for i in li:
+#     x.insert(i)
