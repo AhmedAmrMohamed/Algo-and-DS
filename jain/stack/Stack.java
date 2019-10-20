209fde53be0f12455f7aae73baf15d683afaa1b6
Ahmed Amr <ahmedbonumstelio@gmail.com>
2019-10-21 01:22:24 +0200

    more methods added --> isempty(),clear()

--- jain/stack/Stack.java
+++ jain/stack/Stack.java
@@ -1,4 +1,4 @@
-package stack;
+package ass1;
 public class Stack<gene>
 {
     private gene[] stack;
@@ -8,33 +8,40 @@ public class Stack<gene>
       {
         stack = (gene[])new Object[capacity];
       }
-      public Boolean empty()
-      {
-        return idx==0;
+     private int getcapacity(){return stack.length;}
+     
+	 private void reSize(int newcapacity)
+     {
+         gene[] temp = (gene[]) new Object[newcapacity];
+         for(int i=0;i<idx;++i)
+         	temp[i]=stack[i];
+         stack=temp;
       }
-      private void reSize(int newcapacity)
-        {
-          gene[] temp = (gene[]) new Object[newcapacity];
-          for(int i=0;i<idx;++i)
-            temp[i]=stack[i];
-          stack=temp;
-        }
 
-        public void push(gene item)
-          {
-            if(idx==stack.length)
-              reSize(2*stack.length);
-            stack[idx++]=item;
-          }
-          public gene top()
-          {
-            return stack[idx-1];
-          }
-          public gene pop()
-            {
-              if(idx>1&&idx==capacity/4)
-                reSize(stack.length/2);
-              return stack[--idx];
-            }
-            public int getcapacity(){return stack.length;}
+      public void push(gene item)
+      {
+		if(idx==stack.length)
+			reSize(2*stack.length);
+		stack[idx++]=item;
+      }
+     
+ 	  public gene top()
+      {
+		  return stack[idx-1];
+      }
+      public gene pop()
+	  {
+          if(idx>1&&idx==capacity/4)
+            reSize(stack.length/2);
+		  return stack[--idx];
+	  }
+	  public void clear()
+	  {
+		  reSize(2);
+		  idx = 0;
+	  }  
+	  public Boolean isempty()
+	  {
+		  return idx == 0;
+	  }
   }
